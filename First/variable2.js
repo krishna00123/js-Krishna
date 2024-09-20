08a48a445a5bd582173ba82fb0c918dea10d0046
Krishna Bhattarai <67731789+krishna00123@users.noreply.github.com>
2024-09-20 08:28:32 +0000

    datatypes

--- First/variable2.js
+++ First/variable2.js
@@ -1,2 +1,24 @@
-let k = 111;
-console.log(k)
+"use strick"; //treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs , not browser
+
+console.log(3 + 3);
+
+
+let name = "hitesh"
+let age  = 25
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null ==> standalone
+// undefine => value is not define
+//symbol => to find unique
+
+//object
+
+
+console.log(typeof undefined);
